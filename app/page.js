80131f8f43635ec49cc5c1385c5e074146a702e6
Mohini-kakade <kakademohini21@gmail.com>
2025-03-07 09:35:46 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+    title: "Next.js 13 Starter | Coffee",
+    description:
+        "A Next.js 13 starter project built without using create-next-app.",
+};
+
+
+const Page = () => {
+    return (
+        <>
+            <h1>Hello, Next.js! This is Coffee.</h1>
+        </>
+    );
+};
+
+export default Page;
